69b2b326b439f341b288d67c0062d0acaa4e8a69
aaa147147 <1397117005@qq.com>
2025-04-08 21:30:00 +0800

    修复MQTT启动脚本乱码问题

--- 启动MQTT服务器.bat
+++ 启动MQTT服务器.bat
@@ -1,6 +1,5 @@
 @echo off
 :: Start EMQX MQTT server
-echo ���� EMQX MQTT ������...
 
 :: Set EMQX installation path
 set EMQX_PATH=.\emqx-5.3.0-windows-amd64\bin
@@ -8,7 +7,6 @@ set EMQX_PATH=.\emqx-5.3.0-windows-amd64\bin
 
 :: Start EMQX
 call %EMQX_PATH%\emqx start
-echo EMQX MQTT ������������.
 
 :: Pause to view output, press any key to continue
 pause
